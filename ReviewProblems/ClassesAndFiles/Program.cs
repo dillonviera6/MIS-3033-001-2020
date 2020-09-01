80c36475613b8e7184962f0abb43e4ad8a4a1450
Dillon Viera <dillonviera6@gmail.com>
2020-09-01 18:02:46 -0500

    Completed UML and did date time things

--- ReviewProblems/ClassesAndFiles/Program.cs
+++ ReviewProblems/ClassesAndFiles/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ClassesAndFiles
 {
@@ -6,7 +7,38 @@ namespace ClassesAndFiles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Student> students = new List<Student>();
+
+            Student stud1 = new Student();
+            Console.WriteLine($"{stud1.name} has an id of {stud1.id}");
+            stud1.name = "Billy";
+            stud1.id = 2;
+            students.Add(stud1);
+
+            Student stud2 = new Student("Dillon", 1);
+            students.Add(stud2);
+
+            foreach (var stud in students)
+            {
+                Console.WriteLine(stud);
+            }
+
+            DateTime current = DateTime.Now;
+            DateTime past = Convert.ToDateTime("1/1/2020");
+
+            var result = current - past;
+
+            Console.WriteLine($"Has been {result.Days} days since the new year, which was on a {past.DayOfWeek}");
+            TimeSpan difference = current - stud1.enrollDate;
+            Console.WriteLine($"It has been {difference.TotalMilliseconds} milliseconds since {stud1.name} has enrolled");
+
+            Console.WriteLine($"there are currently {Student.totalStudents} enrolled");
+
+
+            //Console.WriteLine($"{stud1.name} has an id of {stud1.id}");
+            //Console.WriteLine($"{stud2.name} has an id of {stud2.id}");
+
+            Console.ReadKey();
         }
     }
 }
