3a51dc1524f2d91be93a6aef60824b89e380a1c6
Dillon Viera <dillonviera6@gmail.com>
2020-09-03 17:42:03 -0500

    Finished reading file example

---
+++ ReviewProblems/ReadingLinesExample/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingLinesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"‪C:\Users\Dillon\Downloads\Pride and Prejudice by Jane Austen.txt";
+
+            //string entireBook = File.ReadAllText(filePath);
+            string[] allLines = File.ReadAllLines(filePath);
+
+            /*Output 5 lines at a time
+            for (int i = 0; i < allLines.Length; i++)
+            {
+                if (i % 5 == 0 && i != 0)
+                {
+                    Console.ReadKey();
+                }
+                Console.WriteLine(allLines[i]);
+            }
+            */
+
+            //output a chapter at a time
+            foreach (var lines in allLines)
+            {
+                if (lines.ToLower().Contains("Chapter") == true) 
+                {
+                    Console.ReadKey();
+                }
+                Console.WriteLine(lines);
+            }
+            Console.WriteLine(allLines);
+        }
+    }
+}
