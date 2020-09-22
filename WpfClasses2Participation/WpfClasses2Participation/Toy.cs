3974cb59c24de9384e0f4379fe110b4d6983fd52
Dillon Viera <dillonviera6@gmail.com>
2020-09-22 17:16:15 -0500

    Participation progress

---
+++ WpfClasses2Participation/WpfClasses2Participation/Toy.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfClasses2Participation
+{
+    class Toy
+    {
+        public string Manufacturer { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        private string Aisle = string.Empty;
+
+        public Toy()
+        {
+            string manufacturer = Manufacturer;
+            string name = Name;
+            double price = Price;
+            string aisle = Aisle;
+        }
+
+        public string GetAisle()
+        {
+
+        }
+    }
+}
